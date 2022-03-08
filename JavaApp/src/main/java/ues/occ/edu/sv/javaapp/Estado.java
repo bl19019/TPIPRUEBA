136178609cb0db1e8f76b081709dd41c9225bd67
bryan <bryan@LAPTOP-G0815DU8>
2022-03-08 13:53:31 -0600

    Mapeo de la BD

---
+++ JavaApp/src/main/java/ues/occ/edu/sv/javaapp/Estado.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ues.occ.edu.sv.javaapp;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author bryan
+ */
+@Entity
+@Table(name = "estado", catalog = "app1", schema = "public")
+@NamedQueries({
+    @NamedQuery(name = "Estado.findAll", query = "SELECT e FROM Estado e"),
+    @NamedQuery(name = "Estado.findByIdEstado", query = "SELECT e FROM Estado e WHERE e.idEstado = :idEstado"),
+    @NamedQuery(name = "Estado.findByNombre", query = "SELECT e FROM Estado e WHERE e.nombre = :nombre"),
+    @NamedQuery(name = "Estado.findByFechaCreacion", query = "SELECT e FROM Estado e WHERE e.fechaCreacion = :fechaCreacion"),
+    @NamedQuery(name = "Estado.findByObservaciones", query = "SELECT e FROM Estado e WHERE e.observaciones = :observaciones")})
+public class Estado implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_estado", nullable = false)
+    private Integer idEstado;
+    @Column(name = "nombre", length = 155)
+    private String nombre;
+    @Column(name = "fecha_creacion")
+    @Temporal(TemporalType.DATE)
+    private Date fechaCreacion;
+    @Column(name = "observaciones", length = 2147483647)
+    private String observaciones;
+    @OneToMany(mappedBy = "idEstado")
+    private List<ObjetoEstado> objetoEstadoList;
+
+    public Estado() {
+    }
+
+    public Estado(Integer idEstado) {
+        this.idEstado = idEstado;
+    }
+
+    public Integer getIdEstado() {
+        return idEstado;
+    }
+
+    public void setIdEstado(Integer idEstado) {
+        this.idEstado = idEstado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public List<ObjetoEstado> getObjetoEstadoList() {
+        return objetoEstadoList;
+    }
+
+    public void setObjetoEstadoList(List<ObjetoEstado> objetoEstadoList) {
+        this.objetoEstadoList = objetoEstadoList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idEstado != null ? idEstado.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Estado)) {
+            return false;
+        }
+        Estado other = (Estado) object;
+        if ((this.idEstado == null && other.idEstado != null) || (this.idEstado != null && !this.idEstado.equals(other.idEstado))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ues.occ.edu.sv.javaapp.Estado[ idEstado=" + idEstado + " ]";
+    }
+    
+}
---
+++ JavaApp/src/main/java/ues/occ/edu/sv/javaapp/TipoObjeto.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ues.occ.edu.sv.javaapp;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author bryan
+ */
+@Entity
+@Table(name = "tipo_objeto", catalog = "app1", schema = "public")
+@NamedQueries({
+    @NamedQuery(name = "TipoObjeto.findAll", query = "SELECT t FROM TipoObjeto t"),
+    @NamedQuery(name = "TipoObjeto.findByIdTipoObjeto", query = "SELECT t FROM TipoObjeto t WHERE t.idTipoObjeto = :idTipoObjeto"),
+    @NamedQuery(name = "TipoObjeto.findByActivo", query = "SELECT t FROM TipoObjeto t WHERE t.activo = :activo"),
+    @NamedQuery(name = "TipoObjeto.findByFechaCreacion", query = "SELECT t FROM TipoObjeto t WHERE t.fechaCreacion = :fechaCreacion")})
+public class TipoObjeto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_tipo_objeto", nullable = false)
+    private Integer idTipoObjeto;
+    @Column(name = "activo")
+    private Boolean activo;
+    @Column(name = "fecha_creacion")
+    @Temporal(TemporalType.DATE)
+    private Date fechaCreacion;
+    @OneToMany(mappedBy = "idTipoObjeto")
+    private List<Objeto> objetoList;
+
+    public TipoObjeto() {
+    }
+
+    public TipoObjeto(Integer idTipoObjeto) {
+        this.idTipoObjeto = idTipoObjeto;
+    }
+
+    public Integer getIdTipoObjeto() {
+        return idTipoObjeto;
+    }
+
+    public void setIdTipoObjeto(Integer idTipoObjeto) {
+        this.idTipoObjeto = idTipoObjeto;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public List<Objeto> getObjetoList() {
+        return objetoList;
+    }
+
+    public void setObjetoList(List<Objeto> objetoList) {
+        this.objetoList = objetoList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idTipoObjeto != null ? idTipoObjeto.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof TipoObjeto)) {
+            return false;
+        }
+        TipoObjeto other = (TipoObjeto) object;
+        if ((this.idTipoObjeto == null && other.idTipoObjeto != null) || (this.idTipoObjeto != null && !this.idTipoObjeto.equals(other.idTipoObjeto))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ues.occ.edu.sv.javaapp.TipoObjeto[ idTipoObjeto=" + idTipoObjeto + " ]";
+    }
+    
+}
